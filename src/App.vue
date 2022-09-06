506cd1d3aa53dc87a831c7eeccc5dfd3dc8bdd33
李子木 <2358656631qq.com>
2022-09-06 11:16:02 +0800

    feat:项目初始化

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,18 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
-  </div>
+  <div>黑马头条</div>
 </template>
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+// git log 查看commit记录
+// git status 查看当前仓库状态
+// git add .提交到暂存区
+// git commit -m 提交到暂存区
+//
+export default {
+
 }
+</script>
 
-nav {
-  padding: 30px;
+<style>
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
